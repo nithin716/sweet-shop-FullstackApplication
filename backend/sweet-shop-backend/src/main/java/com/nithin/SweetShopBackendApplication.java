aa8370d040dba6182439d0ec7f548f9c2285c2dc
nithin716 <nithinyadav370@gmail.com>
2025-12-13 13:03:35 +0530

    Initialize Spring Boot backend

---
+++ backend/sweet-shop-backend/src/main/java/com/nithin/SweetShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nithin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetShopBackendApplication.class, args);
+	}
+
+}
---
+++ backend/sweet-shop-backend/src/test/java/com/nithin/SweetShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
